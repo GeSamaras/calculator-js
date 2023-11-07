5b603281b140b3168428ceb2999059518142ccfd
GeSamaras <georgiosamaras@hotmail.com>
2023-11-07 18:05:31 +0100

    prototype from frontendmasters 	new file:   images/background.jpg 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,93 +1,97 @@
-let numbers = document.querySelectorAll('.buttons_number');
-let operators = document.querySelectorAll('.buttons_operands');
-let display = document.querySelector('.display_input');
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+const screen = document.querySelector(".screen");
 
-
-// calculator inputs
-let firstNumber = '';
-let secondNumber = '';
-let operator = '';
-let displayValue = '';
-
-
-const displayNumber = number => {
-
-}
-
-
-
-
-
-// buttons section
 function buttonClick(value) {
-  // if the value is not a number when parsedInt, then call the symbol function
   if (isNaN(parseInt(value))) {
     handleSymbol(value);
   } else {
     handleNumber(value);
   }
+  rerender();
 }
 
-
-
-
-// taking the buttons and eventlistening for clicks, then getting
-// the innertext of the divs
-numbers.forEach(number => {
-  number.addEventListener('click', (event) => {
-    buttonClick(event.target.innerText);
-  });
-});
-
-
-// same for the operators
-operators.forEach(operator => {
-  operator.addEventListener('click', (event) => {
-    buttonClick(event.target.innerText);
-  });
-});
-
-
-// handling different types of input
 function handleNumber(value) {
-  console.log('number');
-  if (operator) {
-    secondNumber += value;
-    displayValue = secondNumber;
+  if (buffer === "0") {
+    buffer = value;
   } else {
-    firstNumber += value;
-    displayValue = firstNumber;
+    buffer += value;
   }
-  display.innerText = displayValue;
+}
+
+function handleMath(value) {
+  if (buffer === "0") {
+    // do nothing
+    return;
+  }
+
+  const intBuffer = parseInt(buffer);
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+
+  previousOperator = value;
+
+  buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "-") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "×") {
+    runningTotal *= intBuffer;
+  } else {
+    runningTotal /= intBuffer;
+  }
 }
 
 function handleSymbol(value) {
-  console.log('symbol');
+  switch (value) {
+    case "C":
+      buffer = "0";
+      runningTotal = 0;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        // need two numbers to do math
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = +runningTotal;
+      runningTotal = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case "+":
+    case "-":
+    case "×":
+    case "÷":
+      handleMath(value);
+      break;
+  }
 }
 
-
-/* appendNumber = number => {
-    if (number === '.' && displayValue.includes('.')) return;
-    displayValue = displayValue.toString() + number.toString();
-    display.innerText = displayValue;
-} */
-
-
-
-// operations for the calculator aha
-function operate (operator, firstNumber, secondNumber) {
-    switch (operator) {
-        case '+':
-        return firstNumber + secondNumber;
-        break;
-        case '-':
-        return firstNumber - secondNumber;
-        break;
-        case '*':
-        return firstNumber * secondNumber;
-        break;
-        case '/':
-        return firstNumber / secondNumber;
-        break;
-    }
+function rerender() {
+  screen.innerText = buffer;
 }
+
+function init() {
+  document
+    .querySelector(".calc-buttons")
+    .addEventListener("click", function (event) {
+      buttonClick(event.target.innerText);
+    });
+}
+
+init();
